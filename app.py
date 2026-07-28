50b4da7142d4187a60b87d532d45fc1e326fff5a
chaudharyaishwarya07-coder <chaudharyaishwarya07@gmail.com>
2026-07-28 12:39:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader 
 from PIL import Image 
 # api keys
-GOOGLE= st.sidebar.text_input("GEMINI",type="password")
+GOOGLE= st.sidebar.text_input("GOOGLE",type="password")
 GROQ= st.sidebar.text_input("GROQ",type="password")
 TAVILY =st.sidebar.text_input("TAVILY",type="password")
 if not (GOOGLE) and not (GROQ) and not (TAVILY):
